553b0113c299f122b7b2a20ce3d0e9fea30e9971
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-13 12:48:44 -0700

    add private user-params to create action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to [:users], notice: 'User created!' #is this the right redirect?
+      session[:user_id] = user.id
+      redirect_to '/', notice: 'User created!' #is this the right redirect?
     else
-      render :new
+      redirect_to '/signup' #render :new
     end
   end   
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
