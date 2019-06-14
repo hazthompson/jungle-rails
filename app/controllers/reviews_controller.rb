c5b251f38f4fdb6711b2f3bebaafe4e06574b839
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-14 15:27:27 -0700

    save review form info into review model (db)

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  def create
+    # raise "Yay, I'm here!"
+     
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.build(review_params)
+    @review.user_id = current_user.id
+    puts @review.user_id
+     @review.save!
+    redirect_to product_url(@product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
+end
