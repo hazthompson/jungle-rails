2bf0873617145fe85c35b2ce22ab311804a81b20
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-14 12:35:16 -0700

    validation added for review inputs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
-
   belongs_to :product
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, numericality: {greater_than:0, less_than:6} 
 end
