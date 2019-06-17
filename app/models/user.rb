6c758db9917175a0ea5f6d7950b557fb26625472
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-17 13:08:58 -0700

    add relevant validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, :uniqueness => {:case_sensitive => false}
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
