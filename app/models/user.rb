3073dc8723fa44fb922025f755c0ac08066e4827
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-06-17 14:11:47 -0700

    add validation if no email and password length greater than 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, :uniqueness => {:case_sensitive => false}
-  # validates :password, presence: true
+  validates :email, presence: true
+  validates :password, length: { minimum: 5 }
   validates :password_confirmation, presence: true
 end
